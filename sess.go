4cea2f7f9897f58bea111b2f15a102c9be6dd5b4
ccsexyz <mofaxyz@gmail.com>
2017-02-25 13:11:24 +0800

    move some code to github.com/ccsexyz/rawcon

--- sess.go
+++ sess.go
@@ -1,13 +1,14 @@
 package kcpraw
 
 import (
+	"github.com/ccsexyz/rawcon"
 	"github.com/pkg/errors"
 	kcp "github.com/xtaci/kcp-go"
 )
 
 // DialWithOptions connects to the remote address "raddr" on the network "udp" with packet encryption
 func DialWithOptions(raddr string, block kcp.BlockCrypt, dataShards, parityShards int) (*kcp.UDPSession, error) {
-	conn, err := dialRAW(raddr)
+	conn, err := rawcon.DialRAW(raddr)
 	if err != nil {
 		return nil, errors.Wrap(err, "net.DialRAW")
 	}
@@ -17,9 +18,29 @@ func DialWithOptions(raddr string, block kcp.BlockCrypt, dataShards, parityShard
 // ListenWithOptions listens for incoming KCP packets addressed to the local address laddr on the network "udp" with packet encryption,
 // dataShards, parityShards defines Reed-Solomon Erasure Coding parameters
 func ListenWithOptions(laddr string, block kcp.BlockCrypt, dataShards, parityShards int) (*kcp.Listener, error) {
-	conn, err := listenRAW(laddr)
+	conn, err := rawcon.ListenRAW(laddr)
 	if err != nil {
 		return nil, errors.Wrap(err, "net.ListenRAW")
 	}
 	return kcp.ServeConn(block, dataShards, parityShards, conn)
+}
+
+// SetNoHTTP determines whether to do http obfuscating
+func SetNoHTTP(v bool) {
+	rawcon.SetNoHTTP(v)
+}
+
+// SetHost set http host
+func SetHost(v string) {
+	rawcon.SetHost(v)
+}
+
+// SetDSCP set tos number
+func SetDSCP(v int) {
+	rawcon.SetDSCP(v)
+}
+
+// SetIgnRST if v is true, the tcp rst packet will be ignored
+func SetIgnRST(v bool) {
+	rawcon.SetIgnRST(v)
 }
