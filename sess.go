23c0334a32c8926deeaf570041b8a99d90e7575d
ccsexyz <ccsexyz@hotmail.com>
2017-04-28 10:35:22 +0800

    the address will be checked

--- sess.go
+++ sess.go
@@ -1,6 +1,9 @@
 package kcpraw
 
 import (
+	"fmt"
+	"net"
+
 	"github.com/ccsexyz/rawcon"
 	"github.com/pkg/errors"
 	kcp "github.com/xtaci/kcp-go"
@@ -8,8 +11,24 @@ import (
 
 var raw rawcon.Raw
 
+func checkAddr(addr string) (err error) {
+	host, _, err := net.SplitHostPort(addr)
+	if err != nil {
+		return
+	} else if len(host) == 0 {
+		err = fmt.Errorf("You must set the addr to ip:port")
+	} else if host == "0.0.0.0" {
+		err = fmt.Errorf("You can't set host to 0.0.0.0")
+	}
+	return
+}
+
 // DialWithOptions connects to the remote address "raddr" on the network "udp" with packet encryption
 func DialWithOptions(raddr string, block kcp.BlockCrypt, dataShards, parityShards int) (*kcp.UDPSession, error) {
+	err := checkAddr(raddr)
+	if err != nil {
+		return nil, errors.Wrap(err, "checkAddr")
+	}
 	conn, err := raw.DialRAW(raddr)
 	if err != nil {
 		return nil, errors.Wrap(err, "net.DialRAW")
@@ -20,6 +39,10 @@ func DialWithOptions(raddr string, block kcp.BlockCrypt, dataShards, parityShard
 // ListenWithOptions listens for incoming KCP packets addressed to the local address laddr on the network "udp" with packet encryption,
 // dataShards, parityShards defines Reed-Solomon Erasure Coding parameters
 func ListenWithOptions(laddr string, block kcp.BlockCrypt, dataShards, parityShards int) (*kcp.Listener, error) {
+	err := checkAddr(laddr)
+	if err != nil {
+		return nil, errors.Wrap(err, "checkAddr")
+	}
 	conn, err := raw.ListenRAW(laddr)
 	if err != nil {
 		return nil, errors.Wrap(err, "net.ListenRAW")
